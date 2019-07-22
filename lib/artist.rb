c9fe6e2f5f30a907e9c142c03e3d592b9a77d3cb
Ashley Muller <serenehopes@gmail.com>
2019-07-22 23:16:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,40 @@
 class Artist
-
+  attr_accessor :name
   @@all = []
-  attr_accessor :name, :songs
-   
   def initialize(name)
-    @name = name 
-    @songs = [] 
-    save
-   
+    @name = name
+    @songs = []
   end
-
-  def songs
-    @songs
-  end
-
+  
   def add_song(song)
     @songs << song
   end
-
-  def save
-    self.class.all << self
+  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
   end
-
+  
+  def songs
+    @songs
+  end
+  
+  def self.find_by_name(name)
+    @@all.detect{|artist| artist.name == name}
+  end
+  
+  def save
+    @@all << self
+  end
   def self.all
     @@all
   end
-
-  def self.find_by_name(artist_name)
-    self.all.detect{|artist| artist.name == artist_name} 
-  end
-
-  def self.create_by_name(name)
-    artist = Artist.new(name)
-    artist.name = name
-    artist
-    
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)  
-  end
-
+  
   def print_songs
-    @songs.each {|song| puts song}
-    
+    songs.each {|song| puts song}
+  end
+  def self.create_by_name(name)
+    artist =  self.new(name)
+    artist.save
+    artist
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,25 @@
 class Song
-  attr_accessor :name, :artist, :song_artist
+  attr_accessor :name, :artist
+  @@all = []
   def initialize(name)
     @name = name
   end
-
+  
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-    @artist = artist
-
+    self.artist = artist
+    self
   end
-
-  def self.new_by_filename(name)
-    song_name = name.split(" - ")[1]
-    song_artist = name.split(" - ")[0]
-    
+  
+  def save
+    @@all << self
+  end
+  
+  def self.new_by_filename(filename)
+    artist_name, song_name, genre_name = filename.split(" - ")
     song = Song.new(song_name)
-    song.artist_name=(song_artist)
+    song.artist_name = artist_name
+    song.save
     song
-
   end
 end
